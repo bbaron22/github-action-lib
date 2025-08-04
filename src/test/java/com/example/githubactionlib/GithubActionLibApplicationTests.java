12b100f6f7eb56b82551fda9a104a289378f7cd5
Bruce Baron <bruce.baron@hyland.com>
2025-08-04 13:51:02 -0700

    v0

---
+++ src/test/java/com/example/githubactionlib/GithubActionLibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githubactionlib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionLibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
